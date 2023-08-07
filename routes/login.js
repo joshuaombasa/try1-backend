7929f7570149dd1d8ab9da43c82b08d2ce3c9961
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-07 14:29:34 +0300

    updated login route

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/', async(req, res) => {
-    
+    const { email, password } = req.body
+    console.log(email, password)
+    res.status(200).json({message: 'Request successful'})
 })
 
 module.exports = router
