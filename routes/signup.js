352eec8d66b02e5146d2d645524e17cdf20eaa05
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-07 14:29:16 +0300

    updated signup route

--- routes/signup.js
+++ routes/signup.js
@@ -1,24 +1,28 @@
 const express = require('express')
 const { check,validationResult } = require('express-validator')
+const bcrypt = require('bcrypt')
 const router = express.Router()
 
-router.post('/', [
-    check("email", "please provide a valid email")
-        .isEmail(),
-        check("password", "please provide a password that is greater than 7 characters")
-        .isLength({
-            min: 8
-        })
-] , async(req, res) => {
-    const { email, password} = req.body
 
-    const errors = validationResult(req)
+
+router.post('/', [
+    check("email", "please provide a valid email").isEmail(),
+    check("password").isLength({min : 8})
+       
+], async (req, res) => {
+    const { email,password } = req.body
+
+    const errors =  validationResult(req)
 
     if (!errors.isEmpty()) {
         return res.status(400).json({
-            error : errors.array()
+            errors : errors.array()
         })
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    res.status(200).json({message: "Request successful"})
 })
 
 module.exports = router
