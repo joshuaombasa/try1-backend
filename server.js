74d5f2e0181d839f2dc08985f3ca15d2c7917353
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-07 14:28:36 +0300

    updated server logic

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 
-const signupRoutes = require('./routes/signup')
 const app = express()
 
+const signupRoutes = require('./routes/signup')
+const loginRoutes = require('./routes/login')
+
 app.use(express.json())
+app.use(cors())
 
 app.use('/signup', signupRoutes)
+app.use('/login', loginRoutes)
 
 app.listen(5000, () => {
     console.log('Server running on http://localhost:5000')
