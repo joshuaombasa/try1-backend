919e05c4bc6474b0d7ce54c7f3c785da31938f8e
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-07 19:03:51 +0300

    updated signup route

--- routes/signup.js
+++ routes/signup.js
@@ -1,27 +1,23 @@
 const express = require('express')
-const { check,validationResult } = require('express-validator')
-const bcrypt = require('bcrypt')
-const mysql = require('mysql')
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'try1'
-})
-
 const router = express.Router()
+const bcrypt = require('bcrypt')
+const mysql = require('mysql2/promise')
+const { check, validationResult} = require('express-validator')
 
-
+const dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'try1'
+}
 
 router.post('/', [
-    check("email", "please provide a valid email").isEmail(),
-    check("password").isLength({min : 8})
-       
+    check("email", "Please provide a valid email").isEmail(),
+    check("password", "Please provide a password that is greater than 7 characters").isLength({min: 6})
 ], async (req, res) => {
-    const { firstname, lastname,  phonenumber, email, password} = req.body
+    const { firstname, lastname, phonenumber, email, password } = req.body
 
-    const errors =  validationResult(req)
+    const errors = validationResult(req)
 
     if (!errors.isEmpty()) {
         return res.status(400).json({
@@ -29,21 +25,37 @@ router.post('/', [
         })
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10)
+    //    check if a user exists with a similar email
 
-    const addNewUserSQL = `INSERT INTO users (first_name, last_name, phone_number, email, user_password) VALUES (?,?,?,?,?)`
+    try {
+        const connection = await mysql.createConnection(dbConfig)
+        const checkUserSql = `SELECT * FROM users WHERE email=?`
+        const [rows] = await connection.query(checkUserSql, [email])
 
-    connection.query(
-                     addNewUserSQL, 
-                     [firstname, lastname,  phonenumber, email, hashedPassword],
-                     (error, results) => {
-                        if(error) {
-                            return res.status(400).json(error)
-                        } else if (results) {
-                            res.status(200).json(results)
-                        }
-                     }
-                    )
+        if (rows.length > 0) {
+            connection.end()
+            return res.status(400).json({error: [{" message": "User already exists" }]})
+        }
+
+        // hash the password before adding it to the database
+        const saltRounds = 10
+        const hashedPassword = await bcrypt.hash(password, saltRounds)
+
+        // add new user to the database
+        const addUserSql = `INSERT INTO users (first_name, last_name, phone_number, email, user_password) VALUES (?, ?, ?, ?, ?)`
+
+        await connection.query(
+            addUserSql,
+            [firstname, lastname, phonenumber, email, hashedPassword]
+        )
+        connection.end()
+
+        res.status(200).json({ message: "Signup successful" })
+
+    } catch (error) {
+        res.status(400).json({error : [{" message": error }]})
+    }
+
 })
 
 module.exports = router
