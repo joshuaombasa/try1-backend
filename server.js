463363dba1f360edd4909e94b5f0f0c3302454f4
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-08 11:58:10 +0300

    added admin middleware

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const app = express()
 
 const signupRoutes = require('./routes/signup')
 const loginRoutes = require('./routes/login')
+const adminRoutes = require('./routes/admin')
 
 app.use(express.json())
 app.use(cors())
 
 app.use('/signup', signupRoutes)
 app.use('/login', loginRoutes)
+app.use('/admin', adminRoutes)
 
 app.listen(5000, () => {
     console.log('Server running on http://localhost:5000')
